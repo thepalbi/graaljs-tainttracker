af8d38827d6d1859ab54f8528100ecbbd7af96ad
Pablo Balbi <pabbalbi@amazon.com>
2021-07-03 17:38:00 -0300

    Fixed prop read and knwon sink e2e

--- src/main/java/com/thepalbi/taint/endpoints/KnownSinkEndpoint.java
+++ src/main/java/com/thepalbi/taint/endpoints/KnownSinkEndpoint.java
@@ -18,7 +18,14 @@ public class KnownSinkEndpoint extends FunctionCallEndpoint {
     }
 
     @Override
-    protected void beforeCall(Object receiver, JSFunctionObject function, Object[] arguments) {
+    protected void afterCall(Object receiver, JSFunctionObject function, Object[] arguments, Object result) {
+        // Evaluation taint violations in afterCall, since the argument may be resolved in the following order:
+        // For sth like console.log(obj.f)
+        // 1. invocation console.log - beforeCall
+        // 2. propRead obj.f - beforeEvaluation
+        // 3. propRead obj.f - afterEvaluation
+        // 4. invocation console.log - afterCall
+
         // Maybe the function being called doesn't have a name argument. Use the function object itself to detect if it's a known sink, somehow!
         String functionName = function.getFunctionData().getName();
         if (knownSinkNames.contains(functionName)) {
